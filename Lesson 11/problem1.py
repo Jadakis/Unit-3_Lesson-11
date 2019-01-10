4026a5092e3c7c79606a29132dc70c0ed63816b8
Jadakis <44173632+Jadakis@users.noreply.github.com>
2019-01-10 16:27:38 -0500

    Add files via upload

---
+++ Lesson 11/problem1.py
@@ -0,0 +1,10 @@
+print('-'*65)
+print('Iz iT BrIcK BoT? ')
+print()
+Temperature = input('Temperature in farhrenheit: ')
+Temperature = int(Temperature)
+if Temperature > 32:	
+	print('ItZ not BrIcK, enjoy ya day. ')
+else:
+	print('ItZ BrIcK, wear ya jacket. ')
+print('-'*65) 
---
+++ Lesson 11/problem2.py
@@ -0,0 +1,11 @@
+print('-'*65)
+print('Narrative Bot: ')
+print()
+Student Name = input('Student Name: ')
+Grade = input('Grade: ')
+Grade = int(Grade)
+if Grade >= 65:	
+	print('Narrative: Student Name, Your final grade in AP Computer Science is Grade. You have excelled in all components of the class! I wish you continued success in the next semester of AP Computer Science! ')
+else:
+	print('Narrative: Student Name, Your final grade in AP Computer Science is Grade. This largely, a result of missing projects and homework assignments. Unfortunately, because this grade is less than Grade, you will have to complete an MBA assignment next semester. ')
+print('-'*65) 
